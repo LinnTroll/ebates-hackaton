ba5bfcc13c0a8d331ea61c4b3de3bcb992a4948f
LVIVSOFT\ahomeniuk <ahomeniuk@lvivsoft.com>
2019-03-04 15:55:33 +0200

    add models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class City(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.ForeignKey('core.Country', on_delete=models.CASCADE)
+    timezone = models.TimeField()
+
+
+class Airport(models.Model):
+    name = models.CharField(max_length=100)
+    city = models.ForeignKey('core.City', on_delete=models.CASCADE)
+    country = models.ForeignKey('core.Country', on_delete=models.CASCADE)
+    code = models.CharField(max_length=3)
+    timezone = models.TimeField()
