6124ec8b7a818a7b61468866175b27c9f615c66b
LVIVSOFT\ahomeniuk <ahomeniuk@lvivsoft.com>
2019-03-04 16:14:37 +0200

    update models

--- core/models.py
+++ core/models.py
@@ -2,18 +2,16 @@ from django.db import models
 
 
 class Countries(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
 
 
 class Cities(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     country = models.ForeignKey('core.Country', on_delete=models.CASCADE)
-    timezone = models.CharField()
+    timezone = models.CharField(max_length=32)
 
 
 class Airports(models.Model):
-    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=3, unique=True, primary_key=True)
+    name = models.CharField(max_length=100, unique=True)
     city = models.ForeignKey('core.City', on_delete=models.CASCADE)
-    country = models.ForeignKey('core.Country', on_delete=models.CASCADE)
-    code = models.CharField(max_length=3)
-    timezone = models.CharField()
