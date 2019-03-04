116afc1ad30831fb19a833443cc380ed1c331aa3
LVIVSOFT\ahomeniuk <ahomeniuk@lvivsoft.com>
2019-03-04 16:36:19 +0200

    add first view

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from core.views import MainPage
+from core.views import (
+    AirportsListView,
+)
 
 app_name = 'core'
 
 urlpatterns = [
-    path('', MainPage.as_view(), name='main'),
+    path('', AirportsListView.as_view(), name='airport_list'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,41 @@
-from django.views.generic import TemplateView
+import json
+
+from django.db.models import Q
+from django.http import Http404
+from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.utils.text import slugify
+from django.views.generic import (
+    ListView,
+    UpdateView,
+    CreateView,
+    View,
+)
+
+from core.models import (
+    Countries,
+    Cities,
+    Airports,
+)
 
 
-class MainPage(TemplateView):
-    template_name = 'core/main.html'
+class AirportsListView(ListView):
+    model = Airports
+    type_filtering = True
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        q_param = self.request.GET.get('q')
+        if q_param:
+            queryset = queryset.filter(slug__contains=slugify(q_param))
+        filters = []
+        if filters:
+            queryset = queryset.filter(store_type__in=filters)
+        return queryset.select_related(
+            'code',
+            'name',
+            'city',
+            'country',
+            'timezone',
+        )
