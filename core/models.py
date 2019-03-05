92f8d1475f21bec5cb9a6bd9fc3bd3ee79e6d144
Emil Syvnii <emil.syvnii@gmail.com>
2019-03-05 14:06:24 +0200

    add product recommendations

--- core/models.py
+++ core/models.py
@@ -43,6 +43,28 @@ class FlightCompany(models.Model):
     def cashback(self):
         return sum(ord(s) for s in self.iata) % 5 + 1
 
+
+class Product(models.Model):
+    image = models.ImageField(upload_to='prod_images', blank=False, null=False)
+    store = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    fmp_price = models.DecimalField(decimal_places=2, max_digits=10)
+    regular_price = models.DecimalField(decimal_places=2, max_digits=10)
+    cashback = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.store} - {self.name}'
+
+
+class Recommend(models.Model):
+    product = models.ForeignKey('core.Product', on_delete=models.CASCADE)
+    # country = models.ManyToManyField(Countries)
+
+
+class RecommendCountry(models.Model):
+    country = models.ForeignKey('core.Countries', on_delete=models.CASCADE)
+    recommend = models.ForeignKey('core.Recommend', on_delete=models.CASCADE)
+
 # class Route(models.Model):
 #     src = models.ForeignKey('core.Airports', related_name='route_src', on_delete=models.CASCADE)
 #     dst = models.ForeignKey('core.Airports', related_name='route_dst', on_delete=models.CASCADE)
