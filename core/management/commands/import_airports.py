4d5f200ebc70422f2c0bc0449a5513cc47c7c5de
Pavlo Khudolieiev <pavel.hoodoleev@gmail.com>
2019-03-05 21:59:02 +0200

    update scripts

--- core/management/commands/import_airports.py
+++ core/management/commands/import_airports.py
@@ -1,7 +1,9 @@
-import urllib.request
-from django.core.management.base import BaseCommand, CommandError
-import csv
 import codecs
+import csv
+import urllib.request
+
+from django.core.management.base import BaseCommand
+
 from core.models import Airports, Cities, Countries
 
 API_URL = 'https://raw.githubusercontent.com/jpatokal/openflights/master/data/airports.dat'
@@ -16,39 +18,31 @@ class Command(BaseCommand):
 
         resp = urllib.request.urlopen(API_URL)
         csvfile = list(csv.reader(codecs.iterdecode(resp, 'utf-8')))
-        total_cnt = len(csvfile)
 
         countries = {}
         cities = {}
         airports = []
 
         for n, row in enumerate(csvfile):
-            airport_name = row[1].strip()
-            city_name = row[2].strip()
             country_name = row[3].strip()
-            airport_code = row[4].strip()
-            city_tz = row[11].strip()
 
             if country_name not in countries:
                 countries[country_name] = Countries(name=country_name)
-            country = countries[country_name]
 
         Countries.objects.bulk_create(countries.values())
         countries = {c.name: c for c in Countries.objects.all()}
 
         for n, row in enumerate(csvfile):
-            airport_name = row[1].strip()
             city_name = row[2].strip()
             country_name = row[3].strip()
-            airport_code = row[4].strip()
             city_tz = row[11].strip()
 
             if not city_name or city_name == '\\N':
                 continue
 
             country = countries[country_name]
-            if city_name not in cities:
-                cities[(country_name, city_name)] = Cities(
+            if (country_name.lower(), city_name.lower()) not in cities:
+                cities[(country_name.lower(), city_name.lower())] = Cities(
                     name=city_name,
                     timezone=city_tz,
                     country=country,
@@ -57,19 +51,16 @@ class Command(BaseCommand):
         Cities.objects.bulk_create(cities.values())
         cities = {(c.country.name, c.name): c for c in Cities.objects.all()}
 
-
         for n, row in enumerate(csvfile):
             airport_name = row[1].strip()
             city_name = row[2].strip()
             country_name = row[3].strip()
             airport_code = row[4].strip()
-            city_tz = row[11].strip()
 
             if not airport_code or airport_code == '\\N':
                 continue
 
-            country = countries[country_name]
-            city = cities.get((country_name, city_name))
+            city = cities.get((country_name.lower(), city_name.lower()))
             if city:
                 airports.append(Airports(
                     code=airport_code,
