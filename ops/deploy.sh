9623429544be629bf096b6ffd7c27f3982fea9f1
Pavlo Khudolieiev <pavel.hoodoleev@gmail.com>
2019-03-05 20:27:00 +0200

    add deploy script

---
+++ ops/deploy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+docker build -t gcr.io/qa-edc-ew/hackaton:latest .
+kubectl apply -f ops/kube/create-web-service.yaml
+kubectl apply -f ops/kube/create-web-deployment.yaml
+
+PODS=$(kubectl get pods --namespace=edc-catalog | grep hackaton- | awk '{print $1}')
+kubectl delete pods $PODS
