b899fb522dedbef83ab7e41e254e865e272385a8
LVIVSOFT\ahomeniuk <ahomeniuk@lvivsoft.com>
2019-03-06 10:17:22 +0200

    increase max_length and save short value

--- core/forms.py
+++ core/forms.py
@@ -3,8 +3,8 @@ from django import forms
 
 class FlightSearchForm(forms.Form):
     src = forms.CharField(max_length=3)
-    src_value = forms.CharField(max_length=100)
+    src_value = forms.CharField(max_length=1000)
     dst = forms.CharField(max_length=3)
-    dst_value = forms.CharField(max_length=100)
+    dst_value = forms.CharField(max_length=1000)
     date_from = forms.DateField(input_formats=['%d/%m/%Y'])
     date_to = forms.DateField(input_formats=['%d/%m/%Y'], required=False)
